c713957b603353faeba020b76b614c32eb42120b
Jason Haruska <jason@haruska.com>
2013-02-24 09:54:01 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoolMadness::Application.config.secret_token = '398d9c80a503437bda2b9f42d171fdb19906d18745d7efe072e489e27b4f90d724950e72d806aa28d200d835aeba785bb4f4eaaddef90f40e6bb91acace94847'
