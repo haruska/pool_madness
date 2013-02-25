cef83d5d3592f94a931a0e52f9d948f86ac8ea80
Jason Haruska <jason@haruska.com>
2013-02-24 19:42:10 -0800

    update host for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ PoolMadness::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'pool-madness.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
