b15ef8691e6c78ab43611bc0cf2f1c7caa65ccc2
Jason Haruska <jason@backupify.com>
2014-03-24 12:06:33 -0500

    increase sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,14 @@
 # three unicorns = 3 connections
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
+  config.redis = { :size => 5 }
 end
 
 # so one sidekiq can have 5 connections
 Sidekiq.configure_server do |config|
-  config.redis = { :size => 7 }
+  config.redis = { :size => 35 }
   database_url = ENV['DATABASE_URL']
   if(database_url)
-    ENV['DATABASE_URL'] = "#{database_url}?pool=10"
+    ENV['DATABASE_URL'] = "#{database_url}?pool=20"
     ActiveRecord::Base.establish_connection
   end
 end
