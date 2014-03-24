2982e767ba5684548babc998956932db3d58795c
Jason Haruska <jason@haruska.com>
2014-03-24 13:52:25 -0500

    revert scaling

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,14 @@
 # three unicorns = 3 connections
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 5 }
+  config.redis = { :size => 1 }
 end
 
 # so one sidekiq can have 5 connections
 Sidekiq.configure_server do |config|
-  config.redis = { :size => 35 }
+  config.redis = { :size => 5 }
   database_url = ENV['DATABASE_URL']
   if(database_url)
-    ENV['DATABASE_URL'] = "#{database_url}?pool=20"
+    ENV['DATABASE_URL'] = "#{database_url}?pool=10"
     ActiveRecord::Base.establish_connection
   end
 end
