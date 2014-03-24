988dd2d1b1d312d74d464a26cdaf3bde71d7722c
Jason Haruska <jason@haruska.com>
2014-03-24 17:19:56 -0500

    update brackets first

--- app/workers/update_possible_outcomes.rb
+++ app/workers/update_possible_outcomes.rb
@@ -2,6 +2,8 @@ class UpdatePossibleOutcomes
   include Sidekiq::Worker
 
   def perform
+    Bracket.update_all(:best_possible => 1000)
+
     Sidekiq.redis do |redis|
       PossibleOutcome.generate_all_slot_bits do |sb|
         redis.lpush(CalculatePossibleOutcome::LIST_KEY, sb)
