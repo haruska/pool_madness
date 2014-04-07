0dc555d4859878cd4f4f1b9ffb524f635ff06fd6
Jason Haruska <jason@backupify.com>
2014-04-07 17:20:05 -0500

    make secret token variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PoolMadness::Application.config.secret_token = '398d9c80a503437bda2b9f42d171fdb19906d18745d7efe072e489e27b4f90d724950e72d806aa28d200d835aeba785bb4f4eaaddef90f40e6bb91acace94847'
+
+#You can generate a new one using `rake secret`
+PoolMadness::Application.config.secret_token = ENV['SECRET_TOKEN']
