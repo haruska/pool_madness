5d766477e02240739347a1f7fed40bad4ad8d744
Jason Haruska <jason@haruska.com>
2015-01-15 09:26:31 -0600

    test Pick#points

--- spec/models/pick_spec.rb
+++ spec/models/pick_spec.rb
@@ -1,80 +1,115 @@
 require "spec_helper"
 
 describe Pick, type: :model do
-  subject { create(:pick) }
+  before(:all) { build(:tournament) }
+  let(:bracket) { create(:bracket, :completed) }
 
-  it "has a valid factory" do
-    expect(subject).to be_valid
-  end
+  subject { bracket.picks.first }
 
   it { should validate_presence_of(:game) }
   it { should validate_presence_of(:bracket) }
 
   context "accessible attributes" do
-    let(:game) { create(:game) }
-    let(:team) { create(:team) }
-    let(:bracket) { create(:bracket) }
-
-    it "allows updating of the game and team" do
-      subject.update_attributes(game_id: game.id, team_id: team.id)
-      subject.reload
-
-      expect(subject.game).to eq(game)
-      expect(subject.team).to eq(team)
-    end
-
     it "does not allow updating of the bracket" do
       expect { subject.update_attributes(bracket_id: bracket.id) }.to raise_exception
     end
   end
 
-  context "#first_team" do
+  context "#first_team / #second_team" do
+    subject { bracket.picks.find_by_game_id(game.id) }
+
     context "game is in the first round" do
-      it "is the team_one of the associated game"
+      let(:game) { Game.round_for(1, Team::REGIONS.first).sample }
+
+      it "is the teams of the associated game" do
+        expect(subject.first_team).to eq(game.team_one)
+        expect(subject.second_team).to eq(game.team_two)
+      end
     end
 
     context "game is in subsequent rounds" do
-      it "is the winning team of this brackets previous pick (game_one)"
-    end
-  end
+      let(:game) { Game.round_for(2, Team::REGIONS.first).sample }
+      let(:previous_picks) { [game.game_one_id, game.game_two_id].map {|game_id| bracket.picks.find_by_game_id(game_id) } }
 
-  context "#second_team" do
-    context "game is in the first round" do
-      it "is the team_two of the associated game"
-    end
-
-    context "game is in subsequent rounds" do
-      it "is the winning team of this brackets previous pick (game_two)"
+      it "is the winning team of this brackets previous picks" do
+        expect(subject.first_team).to eq(previous_picks.first.team)
+        expect(subject.second_team).to eq(previous_picks.second.team)
+      end
     end
   end
 
   context "#points" do
-    context "with a possible game" do
-      it "uses the outcome of the passed in game"
-    end
+    let(:expected_points) { Pick::POINTS_PER_ROUND[game.round] + subject.team.seed }
 
-    context "using the actual game" do
-      it "uses the actual outcome of the associated game"
+    context do
+      before do
+        #set the winner
+        game.score_one = 1
+        game.score_two = 2
+        game.score_one = 3 if game.winner != subject.team
+      end
+
+      context "with a possible game" do
+        let(:game) { Game.new(team_one: subject.first_team, team_two: subject.second_team) }
+
+        it "uses the outcome of the passed in game" do
+          expect(subject.points(game)).to eq(expected_points)
+        end
+      end
+
+      context "using the actual game" do
+        let(:game) { subject.game }
+
+        it "uses the actual outcome of the associated game" do
+          expect(subject.points).to eq(expected_points)
+        end
+      end
     end
 
     context "there is a picked team" do
-      context "and someone has one the associated game" do
+      let(:game) { subject.game }
+
+      context "and someone has won the associated game" do
+        before do
+          #set the winner
+          game.score_one = 1
+          game.score_two = 2
+        end
+
         context "and the pick is correct" do
-          it "is the team's seed plus the points for the round"
+          before do
+            game.score_one = 3 if game.winner != subject.team
+          end
+
+          it "is the team's seed plus the points for the round" do
+            expect(subject.points).to eq(Pick::POINTS_PER_ROUND[game.round] + subject.team.seed)
+          end
         end
 
         context "and the pick is incorrect" do
-          it "is zero"
+          before do
+            game.score_one = 3 if game.winner == subject.team
+          end
+
+          it "is zero" do
+            expect(subject.points).to eq(0)
+          end
         end
       end
 
       context "there is not a winner yet" do
-        it "is zero"
+        it "is zero" do
+          expect(subject.points).to eq(0)
+        end
       end
     end
 
     context "team is nil" do
-      it "is zero"
+      before { subject.team_id = nil }
+
+      it "is zero" do
+        expect(subject.points).to eq(0)
+      end
     end
   end
 
