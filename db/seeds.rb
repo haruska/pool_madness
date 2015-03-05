451271474391128f67fa0ada6a11bb65f7d1d78a
Jason Haruska <jason@haruska.com>
2015-03-05 10:57:30 -0600

    remove using database cleaner in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# Environment variables (ENV['...']) are set in the file config/application.yml.
-# See http://railsapps.github.com/rails-environment-variables.html
 
-DatabaseCleaner.strategy = :truncation
-DatabaseCleaner.clean
+#DatabaseCleaner.clean_with :truncation
+
+
+User.delete_all
+Tournament.delete_all
+PoolUser.delete_all
+Pool.delete_all
+Bracket.delete_all
 
 user = User.create_with(name: ENV['ADMIN_NAME'].dup, password: ENV['ADMIN_PASSWORD'].dup, password_confirmation: ENV['ADMIN_PASSWORD'].dup).find_or_create_by(email: ENV['ADMIN_EMAIL'].dup)
 user.admin!
 puts "admin user: #{user.name}"
-
 
 [4.days.from_now, 4.days.ago].each do |tip_off|
   puts "creating tournament with tip off #{tip_off}"
