58aa5be2019b431214816a5f54e1cb6a89de8782
Jason Haruska <jason@haruska.com>
2015-03-18 18:28:28 -0500

    disable scheduler and fetch game scores

--- app/workers/scheduler.rb
+++ app/workers/scheduler.rb
@@ -2,7 +2,7 @@ class Scheduler
   include Sidekiq::Worker
 
   def perform
-    FetchGameScores.perform_async
-    Scheduler.perform_in(1.minute)
+    #FetchGameScores.perform_async
+    #Scheduler.perform_in(1.minute)
   end
 end
