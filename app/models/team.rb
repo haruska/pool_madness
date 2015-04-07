cf9c22b9632fe0d9720e8557502637b3a6f5007e
Jason Haruska <jason@haruska.com>
2015-04-07 08:09:09 -0400

    Update team.rb

--- app/models/team.rb
+++ app/models/team.rb
@@ -22,6 +22,7 @@ class Team < ActiveRecord::Base
 
   def still_playing?
     game = first_game
+    return false unless game.present?
     while game.winner.present?
       return false if game.winner != self
       game = game.next_game
