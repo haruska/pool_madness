f752e68e858026ebc0e9002b23dbe1111fe746a3
Jason Haruska <jason@haruska.com>
2015-05-01 09:09:54 -0500

    only use hirefire in production

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,13 +1,15 @@
-HireFire::Resource.configure do |config|
-  config.dyno(:all_worker) do
-    HireFire::Macro::Sidekiq.queue
-  end
+if Rails.env.production?
+  HireFire::Resource.configure do |config|
+    config.dyno(:all_worker) do
+      HireFire::Macro::Sidekiq.queue
+    end
 
-  config.dyno(:elimination) do
-    HireFire::Macro::Sidekiq.queue(:elimination)
-  end
+    config.dyno(:elimination) do
+      HireFire::Macro::Sidekiq.queue(:elimination)
+    end
 
-  config.dyno(:fast) do
-    HireFire::Macro::Sidekiq.queue(:default, :mailers, :scores)
+    config.dyno(:fast) do
+      HireFire::Macro::Sidekiq.queue(:default, :mailers, :scores)
+    end
   end
 end
