5e92be54bf19aee4cffe1c692dcf988545fddc69
Jason Haruska <jason@haruska.com>
2015-06-30 13:21:01 -0500

    account for sweet 16 tournaments

--- db/migrate/20150630141427_add_starting_slot_to_team.rb
+++ db/migrate/20150630141427_add_starting_slot_to_team.rb
@@ -8,15 +8,16 @@ class AddStartingSlotToTeam < ActiveRecord::Migration
     sort_order = [1, 16, 8, 9, 5, 12, 4, 13, 6, 11, 3, 14, 7, 10, 2, 15]
     regions = ["South", "West", "East", "Midwest"]
 
-    starting_slot = 64
-
-    regions.each do |region|
-      sort_order.each do |seed|
-        Tournament.all.each do |tournament|
+    Tournament.all.each do |tournament|
+      starting_slot = tournament.num_rounds == 6 ? 64 : 16
+      regions.each do |region|
+        sort_order.each do |seed|
           team = Team.find_by(region: region, seed: seed, tournament_id: tournament.id)
-          team.update(starting_slot: starting_slot)
+          if team.present?
+            team.update(starting_slot: starting_slot)
+            starting_slot += 1
+          end
         end
-        starting_slot += 1
       end
     end
   end
