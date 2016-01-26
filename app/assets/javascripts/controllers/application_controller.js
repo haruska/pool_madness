b5c010c209ed532acb4ba309ede2e5e44d5b4c2e
Jason Haruska <jason@haruska.com>
2016-01-26 10:19:08 -0600

    new menu

--- app/assets/javascripts/controllers/application_controller.js
+++ app/assets/javascripts/controllers/application_controller.js
@@ -1,2 +1,23 @@
 App.createController("Application", {
+  all: function() {
+    this.cacheElements();
+    this.registerEvents();
+  },
+
+  cacheElements: function() {
+    this.$slidingButton = $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close');
+    this.$slidingContent = $('.sliding-panel-content,.sliding-panel-fade-screen');
+  },
+
+  registerEvents: function() {
+    this.$slidingButton.on('click touchstart', this.toggleHamburger);
+
+  },
+
+  toggleHamburger: function(e) {
+    this.$slidingContent.toggleClass('is-visible');
+    e.preventDefault();
+  }
 });
+
+
