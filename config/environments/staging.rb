4c80646dcbe178202e2347ea97d3e60ab0df7a30
Jason Haruska <jason@haruska.com>
2016-03-10 10:39:11 -0600

    configure dalli store in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -39,7 +39,14 @@ PoolMadness::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
   # config.cache_store = :redis_store, "#{ENV['REDISTOGO_URL']}/0/cache", { expires_in: 90.minutes }
-  config.cache_store = :dalli_store
+  config.cache_store = :dalli_store,
+      (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+      {:username => ENV["MEMCACHIER_USERNAME"],
+       :password => ENV["MEMCACHIER_PASSWORD"],
+       :failover => true,
+       :socket_timeout => 1.5,
+       :socket_failure_delay => 0.2
+      }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -81,4 +88,6 @@ PoolMadness::Application.configure do
   config.eager_load = true
 
   config.active_job.queue_adapter = :sidekiq
+
+
 end
