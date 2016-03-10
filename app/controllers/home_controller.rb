4d9c523b6385ad149012cc5fd2595bcdf93003d2
Jason Haruska <jason@haruska.com>
2016-03-10 10:43:04 -0600

    add before action on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,19 @@
 class HomeController < ApplicationController
+  before_action :check_authentication, only: :index
+
   caches_action :index
-  
+
   def index
-    redirect_to pools_path if current_user.present?
   end
 
   def subscribe
     Newsletter.home_page_email(params[:email]).deliver_later
     redirect_to root_path, notice: "Thank you for your interest. We'll contact you when the beta is ready."
+  end
+
+  private
+
+  def check_authentication
+    redirect_to pools_path if current_user.present?
   end
 end
