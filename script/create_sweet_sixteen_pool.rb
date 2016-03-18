493509ea467a2027dc9f6b36a0d648b7aac66f51
Jason Haruska <jason@haruska.com>
2016-03-18 11:55:44 -0500

    script to create sweet sixteen pools

--- script/create_sweet_sixteen_pool.rb
+++ script/create_sweet_sixteen_pool.rb
@@ -1,37 +1,38 @@
-
-haruska_pool = Pool.find_by name: "Haruska"
-haruska_tournament = haruska_pool.tournament
+sixty_four_tournament = Tournament.find_by name: Time.now.year.to_s + " NCAA March Madness Tournament"
 
 tournament = Tournament.create(
-  name: "2015 NCAA Sweet Sixteen",
-  tip_off: haruska_tournament.tip_off + 1.week,
-  eliminating_offset: 0,
-  num_rounds: 4
+    name: Time.now.year.to_s + " NCAA Sweet Sixteen",
+    tip_off: sixty_four_tournament.tip_off + 1.week,
+    num_rounds: 4
 )
 
-game_map = {}
 
-haruska_tournament.round_for(3).each do |game|
-  teams = [game.first_team, game.second_team].map do |team|
-    tournament.teams.create(name: team.name,
-                            score_team_id: team.score_team_id,
-                            seed: team.seed,
-                            region: team.region)
-  end
-
-  g = tournament.games.create(team_one_id: teams.first.id, team_two_id: teams.second.id)
-  game_map[game.id] = g.id
+sixty_four_tournament.rounds.where("number > ?", 2).order(:number).each_with_index do |sf_round, i|
+  tournament.rounds.create(
+      number: i + 1,
+      name: sf_round.name,
+      start_date: sf_round.start_date,
+      end_date: sf_round.end_date
+  )
 end
 
-(4..6).each do |round_num|
-  haruska_tournament.round_for(round_num).each do |game|
-    g = tournament.games.create(game_one_id: game_map[game.game_one_id], game_two_id: game_map[game.game_two_id])
-    game_map[game.id] = g.id
+sixty_four_tournament.round_for(3).each do |game|
+  [game.first_team, game.second_team].each_with_index do |team, i|
+    tournament.teams.create(
+                        region: team.region,
+                        seed: team.seed,
+                        name: team.name,
+                        score_team_id: team.score_team_id,
+                        starting_slot: i == 0 ? game.left_position : game.right_position
+    )
   end
 end
 
-pool = Pool.create(name: "Sweet 16", tournament: tournament)
 
-haruska_pool.pool_users.each do |pool_user|
-  pool.pool_users.create(user_id: pool_user.user_id, role: pool_user.role)
+sixty_four_tournament.pools.each do |previous_pool|
+  pool = tournament.pools.create(name: previous_pool.name + " Sweet 16")
+
+  previous_pool.pool_users.each do |pool_user|
+    pool.pool_users.create(user_id: pool_user.user_id, role: pool_user.role)
+  end
 end
