ea3bd5f55eafdb4fc285d578e54499c8ceaac63a
Jason Haruska <jason@haruska.com>
2016-06-23 14:32:52 -0500

    remove email address from user type

--- app/api/queries/user_type.rb
+++ app/api/queries/user_type.rb
@@ -7,7 +7,6 @@ module Queries
     global_id_field :id
 
     field :model_id, !types.Int, property: :id
-    field :email, !types.String
     field :name, !types.String
   end
 end
--- spec/api/queries/user_type_spec.rb
+++ spec/api/queries/user_type_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Queries::UserType do
   subject { Queries::UserType }
 
   context "fields" do
-    let(:fields) { %w(id model_id email name) }
+    let(:fields) { %w(id model_id name) }
 
     it "has the proper fields" do
       expect(subject.fields.keys).to match_array(fields)
