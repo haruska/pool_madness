c8deb72475af62c1f834ed770b6b3ea10e71777a
Jason Haruska <jason@haruska.com>
2016-09-21 10:02:07 -0500

    allow for better js import paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
-var webpack = require('webpack');
+var webpack = require('webpack')
+var path = require('path')
 
 module.exports = {
   devtool: "source-map",
-  entry: ["./web_client/main.js"],
+  entry: "./web_client/main.js",
   output: {
     path: "./app/assets/javascripts/build",
     filename: "bundle.js"
+  },
+  resolve: {
+    modules: [
+      path.resolve('./web_client'),
+      path.resolve('./node_modules')
+    ]
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: "babel"
       }, {
         test: /\.json$/,
         loader: 'json-loader'
