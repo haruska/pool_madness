6397d98c6a9960f3d16cbec91752f00df1aee270
Jason Haruska <jason@haruska.com>
2017-03-14 08:04:02 -0500

    redirect to login if not authenticated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   respond_to :json
+
   skip_before_filter :verify_authenticity_token
-  before_action :authenticate_user!, only: [:graphql]
+  before_action :authenticate_user!
 
   def home
     respond_to { |f| f.html { render layout: "pages" } }
@@ -17,20 +18,10 @@ class PagesController < ApplicationController
         context: {
           current_user: current_user,
           current_ability: current_ability,
-          optics_agent: env[:optics_agent].with_document(query_string)
+          optics_agent: env[:optics_agent].try(:with_document, query_string)
         }
     )
 
     render json: result
-  end
-
-  if Rails.env.development?
-    def authed_graphql
-      render json: GraphqlSchema.execute(
-        params[:query],
-          variables: params[:variables],
-          context: { current_user: current_user || User.last }
-      )
-    end
   end
 end
--- config/initializers/optics_agent.rb
+++ config/initializers/optics_agent.rb
@@ -1,3 +1,5 @@
-optics_agent = OpticsAgent::Agent.new
-optics_agent.configure { schema GraphqlSchema }
-Rails.application.config.middleware.use optics_agent.rack_middleware
+unless Rails.env.test?
+  optics_agent = OpticsAgent::Agent.new
+  optics_agent.configure { schema GraphqlSchema }
+  Rails.application.config.middleware.use optics_agent.rack_middleware
+end
